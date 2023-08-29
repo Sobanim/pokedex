6474f7f6ed1fdc2fc3cd2e925901e7f32249e79b
Sobanim <sobolev.dmirtiy@gmail.com>
2023-08-29 20:53:49 +0200

    Add images for pokemons

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,13 +1,18 @@
-import {IndexedPokemon} from "../interfaces/pokemon.interfaces";
+import {ListPokemon} from "../interfaces/pokemon.interfaces";
 
 interface PokemonListProps {
-    pokemons: IndexedPokemon[]
+    pokemons: ListPokemon[]
 }
 const PokemonList = ({ pokemons }: PokemonListProps) => {
     return (
         <>
             {pokemons.length > 0 ? pokemons.map( (p) => {
-                return <div key={p.name}>{p.name}</div>
+                return (
+                    <div key={p.name}>
+                        <img src={p.image} alt={p.name} title={p.name}/>
+                        {p.name}
+                    </div>
+                )
             }) : null}
         </>
 
--- src/hooks/usePokemons.ts
+++ src/hooks/usePokemons.ts
@@ -1,17 +1,32 @@
 import {useEffect, useState} from "react";
-import {IndexedPokemon, PokemonListResponce} from "../interfaces/pokemon.interfaces";
-import {POKEMON_API_POKEMON_URL} from "../constants";
+import {IndexedPokemon, ListPokemon, PokemonListResponce} from "../interfaces/pokemon.interfaces";
+import {POKEMON_API_POKEMON_URL, POKEMON_IMG_BASE_URL} from "../constants";
 import {httpClient} from "../api/httpClient";
 
 const usePokemons = () => {
-    const [pokemons, setPokemons] = useState<IndexedPokemon[]>([])
+    const [pokemons, setPokemons] = useState<ListPokemon[]>([])
     const [nextUrl, setNextUrl] = useState<string | null>(POKEMON_API_POKEMON_URL)
 
     const fetchPokemon = async () => {
         if (nextUrl) {
             const result = await httpClient.get<PokemonListResponce>(nextUrl)
-            if (result?.data?.results) setPokemons(result.data.results)
+            if (result?.data?.results) {
+                const listPokemons = result.data.results.map (p => indxPokemonToListPokemon(p))
+                setPokemons(listPokemons)
+            }
         }
+    }
+
+    const indxPokemonToListPokemon = (indexedPokemon: IndexedPokemon) => {
+        const pokedexNumber = parseInt(indexedPokemon.url.replace(`${POKEMON_API_POKEMON_URL}/`, "").replace("/", ""))
+
+        const listPokemon: ListPokemon = {
+            name: indexedPokemon.name,
+            url: indexedPokemon.url,
+            image: `${POKEMON_IMG_BASE_URL}/${pokedexNumber}.gif`,
+            pokedexNumber: pokedexNumber
+        }
+        return listPokemon
     }
 
     useEffect( () => {
