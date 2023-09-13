2134555c314836e0b91d37aaf578eeb1c762b00e
Sobanim <sobolev.dmirtiy@gmail.com>
2023-09-13 14:16:05 +0200

    Fix error with images and generating color

--- src/constants.ts
+++ src/constants.ts
@@ -16,8 +16,11 @@ import {
 
 export const POKEMON_API_BASE_URL = "https://pokeapi.co/api/v2"
 export const POKEMON_API_POKEMON_URL = `${POKEMON_API_BASE_URL}/pokemon`
-export const POKEMON_IMG_BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/"
+// export const POKEMON_IMG_BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/"
 // export const POKEMON_IMG_BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/"
+
+// Work images
+export const POKEMON_IMG_BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
 
 export const POKEMON_TYPES: IndexedType[] = [
     {
